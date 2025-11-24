cfb2b569a176ba365a62fedff2a95b61fe5ac6bf
Michelle Marie P. Habon <michellemariehabon846@gmail.com>
2025-11-24 14:19:08 +0800

    feat: initial setup for income

--- src/util/apiEndpoints.js
+++ src/util/apiEndpoints.js
@@ -1,5 +1,5 @@
-export const BASE_URL = "https://equitrack-backend-kh8q.onrender.com/api/v1.0";
-//export const BASE_URL = "http://localhost:8080/api/v1.0";
+//export const BASE_URL = "https://equitrack-backend-kh8q.onrender.com/api/v1.0";
+export const BASE_URL = "http://localhost:8080/api/v1.0";
 const CLOUDINARY_CLOUD_NAME = "doxx8vaou";
 
 export const API_ENDPOINTS = {
@@ -9,5 +9,6 @@ export const API_ENDPOINTS = {
     GET_ALL_CATEGORIES: "/categories",
     ADD_CATEGORY: "/categories",
     UPDATE_CATEGORY: (categoryId) => `/categories/${categoryId}`,
+    GET_ALL_INCOMES: "/incomes",
     UPLOAD_IMAGE: `https://api.cloudinary.com/v1_1/${CLOUDINARY_CLOUD_NAME}/image/upload`
 }
